2d72a7e0b228f0e7b1c19574bc8a74cae4def9ed
Lewen ZHAO <zhaolewen@gmail.com>
2020-05-10 23:26:57 +0200

    reorg 2

--- LinkURL.py
+++
@@ -1,1 +0,0 @@
-main_html = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={0}&type={1}&dateb=&owner=exclude&count=100"
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='py-sec-xbrl',
+     version='0.1',
+     scripts=['py-sec-xbrl'] ,
+     author="Lewen Zhao",
+     author_email="zhaolewen@gmail.com",
+     description="SEC Edgar Parser based on Python 3",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/zhaolewen/py-sec-xbrl",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
