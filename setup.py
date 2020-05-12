0d9cdb24c71f889f459cc5658fb16bcc03d6184e
Lewen ZHAO <zhaolewen@gmail.com>
2020-05-12 20:07:46 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='py-sec-xbrl',
-     version='0.1',
+     version='0.2',
      author="Lewen Zhao",
      author_email="zhaolewen@gmail.com",
      description="SEC EDGAR Parser based on Python 3",
